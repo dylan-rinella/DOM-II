408a63a4d22b8f6403cb0827f3e107b91dbea7fa
dylan-rinella <rinelladylan@aim.com>
2020-12-01 14:46:46 -0500

    halfway

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+const html = document.querySelector('html')
+
+const busPic = document.querySelector('header img')
+
+busPic.addEventListener('click', (event) =>{
+  event.target.style.border = 'solid red';
+})
+
+const logoHeading = document.querySelector('.logo-heading')
+
+logoHeading.addEventListener('click', (event) => {
+  event.target.style.color = 'orange'
+})
+
+const backgroundColorSwap = document.querySelector('body');
+
+backgroundColorSwap.addEventListener('click', (event) => {
+  event.target.style.color = 'green'
+})
+
+document.addEventListener('keydown', function(event){
+  if(event.key === 'Escape'){
+    backgroundColorSwap.style.color = 'purple'
+  }
+})
+
+document.addEventListener('keydown', function(event){
+  if(event.key === '~'){
+    html.style.border = 'solid green'
+  }
+})
